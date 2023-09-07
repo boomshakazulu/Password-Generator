1d6cea0696d5da25d77b86b1479720c807dff890
boomshakazulu <dallonlahoda@hotmail.com>
2023-09-07 03:46:07 -0600

    added math to js

--- script.js
+++ script.js
@@ -15,35 +15,36 @@ generateBtn.addEventListener("click", writePassword);
 
 // popup windows for password selections
 function generatePassword() {
-var passwordLength = prompt("How many Characters do you want?<br> (between 8-128)")
+var passwordLength = prompt("How many Characters do you want?(between 8-128)")
+//failure on invalid length
 if (passwordLength < 8 || passwordLength > 128){
-  alert("Please insert a valid length between 8-128");  
+  alert("Please insert a valid length between 8-128");
+    return;
 } else {
 var lowerCase = confirm ("Would you like lowercase letters?")
 var upperCase = confirm ("Would you like uppercase letters?")
 var numbers = confirm("Would you like to include numbers?")
 var specialChar = confirm ("Would you like to include special characters?")
-}
-
+//variables
 var letters = "abcdefghijklmnopqrstuvwxyz";
 var capitals = letters.toUpperCase();
 var numbersaray = "0123456789";
 var special = "!@#$%^&*()";
-
+//variable groups
 var allChars = letters+capitals+numbersaray+special;
 var lowUpNumber=letters+capitals+numbersaray;
 var lowUpSpecial = letters+capitals+special;
-var lowSpecNumber= letters+special+numbers;
-var upNumbSpecial =capitals+numbers+special;
+var lowSpecNumber= letters+special+numbersaray;
+var upNumbSpecial =capitals+numbersaray+special;
 var lowUpper = letters+capitals;
-var lowNumber = letters +numbers;
+var lowNumber = letters +numbersaray;
 var lowSpecial = letters+special;
-var upNumber = capitals+numbers;
+var upNumber = capitals+numbersaray;
 var upSpecial= capitals+special;
-var numSpecial = numbers+special;
+var numSpecial = numbersaray+special;
 
 let randomPassword = "";
-
+//nested forstatement for password length
 for(var i=0; i<parseInt(passwordLength); i++){
  if (lowerCase&&upperCase&&numbers&&specialChar){
   let randomNumber = Math.floor(Math.random()* allChars.length + 1);
@@ -61,40 +62,57 @@ for(var i=0; i<parseInt(passwordLength); i++){
     var randomNumber = Math.floor(Math.random() * lowSpecNumber.length);
     randomPassword += lowSpecNumber.substring(randomNumber, randomNumber +1);
   
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (upperCase&&numbers&&specialChar){
     var randomNumber = Math.floor(Math.random() * upNumbSpecial.length);
     randomPassword += upNumbSpecial.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (lowerCase&&upperCase){
     var randomNumber = Math.floor(Math.random() * lowUpper.length);
     randomPassword += ulowUpper.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (lowerCase&&numbers){
     var randomNumber = Math.floor(Math.random() * lowNumber.length);
     randomPassword += lowNumber.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (lowerCase&&specialChar){
     var randomNumber = Math.floor(Math.random() * lowSpecial.length);
     randomPassword += lowSpecial.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (upperCase&&numbers){
     var randomNumber = Math.floor(Math.random() * upNumber.length);
     randomPassword += upNumber.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (upperCase&&specialChar){
     var randomNumber = Math.floor(Math.random() * upSpecial.length);
     randomPassword += upSpecial.substring(randomNumber, randomNumber +1);
 
-  }else if (lowerCase&&numbers&&specialChar){
+  }else if (numbers&&specialChar){
     var randomNumber = Math.floor(Math.random() * numSpecial.length);
     randomPassword += numSpecial.substring(randomNumber, randomNumber +1);
 
+  }else if (lowerCase){
+    var randomNumber = Math.floor(Math.random() * letters.length);
+    randomPassword += letters.substring(randomNumber, randomNumber +1);
+
+  }else if (upperCase){
+    var randomNumber = Math.floor(Math.random() * capitals.length);
+    randomPassword += capitals.substring(randomNumber, randomNumber +1);
+
+  }else if (numbers){
+    var randomNumber = Math.floor(Math.random() * numbersaray.length);
+    randomPassword += numbersaray.substring(randomNumber, randomNumber +1);
+
+  }else if (specialChar){
+    var randomNumber = Math.floor(Math.random() * specialChar.length);
+    randomPassword += specialChar.substring(randomNumber, randomNumber +1);
 
   }else {
     alert("Please select at least 1 option")
+    return "invalid"
   }
 }
 return randomPassword;
+}
 }
 
 
